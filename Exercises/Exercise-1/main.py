e68898901e93b0a1c84fddffa9c656f1aab2367f
Long Kim <koniree2309@gmail.com>
2024-05-28 04:42:35 -0400

    Yay

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,20 +1,65 @@
 import requests
+import os
+import glob
+import zipfile
+from io import BytesIO
+import logging
+import shutil
+
 
 download_uris = [
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+DOWNLOAD_DIR = 'Downloads'
+CURRENT_DIR = os.getcwd()
+
+def create_dir():
+    dir_path = os.path.join(CURRENT_DIR, DOWNLOAD_DIR)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    return dir_path
+    
+def read_url(url, dir):
+    try:    
+        response = requests.get(url)
+        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code.
+    except requests.RequestException as e:
+        logging.error(f"Failed to download the file from {url}: {e}")
+        return
+
+    filename = url.split('/')[-1]
+    with open(filename, 'wb') as file:
+        zip_file = zipfile.ZipFile(BytesIO(filename.content)) 
+        unzip = zip_file.extractall(dir)
+    os.remove(zip_file)
+
+def remove_zip_files():
+    # remove zip files
+    for f in glob.glob("*.zip"):
+        os.remove(f)
+
+def remove_csv_files():
+    # remove csv files
+    try:
+        shutil.rmtree('./Downloads/')
+    except FileNotFoundError or OSError:
+        pass
 
 def main():
-    # your code here
-    pass
+    remove_csv_files()
+    remove_zip_files()
+    create_dir()
+    for url in download_uris:
+        read_url(url, './Downloads')
+    print(os.listdir('./Downloads'))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,12 +1,55 @@
 import requests
 import pandas
+import io
 
+
+url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+from bs4 import BeautifulSoup
+
+def web_scrape(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table')
+    rows = soup.find_all('tr')
+    values = []
+    target_date = '2024-01-19 10:21'
+    for row in rows:
+        cells = row.find_all('td')
+        for cell in cells:
+            if target_date in cell.text:
+                return row
+            
+def read_csv(content):
+    content = '<html>'+str(content)+'</html>'
+    bs = BeautifulSoup(content, 'html.parser')
+    csv_link = bs.find('a', href=True)  # Find the first <a> tag with an href attribute
+
+    if csv_link:
+        csv_path = csv_link['href']
+        csv_url = url + csv_path  # Extract the URL or path to the CSV file
+        try:
+            csv_df = pandas.read_csv(csv_url)
+            
+            return csv_df
+
+        except Exception as e:
+            print(f"Error reading CSV: {e}")
+    else:
+        print("No link found")
+
+    
+        
+    
 
 
 def main():
-    # your code here
-    pass
+    html_content = web_scrape(url=url)
+    csvFile = read_csv(html_content)
+    csv_file = csvFile.to_csv("./csvfile.csv")
+    print(csvFile['HourlyDryBulbTemperature'].max())
 
+    
+    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
